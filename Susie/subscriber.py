05b990b8d2eb397262dffac3d89a5dd6d5ebae89
susietsx0503 <susietan@g.ucla.edu>
2019-12-08 18:10:55 +0000

    mqtt

---
+++ Susie/subscriber.py
@@ -0,0 +1,72 @@
+
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21,GPIO.OUT)
+
+MQTT_SERVER = "2605:e000:1703:634e:c500:ae55:c2a:9d3e"
+MQTT_PATH = "test_channel"
+
+# The callback for when the client receives a connect response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_PATH)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    GPIO.output(21,GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(21,GPIO.LOW)
+	time.sleep(1)
+    # more callbacks, etc
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(MQTT_SERVER, 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21,GPIO.OUT)
+
+MQTT_SERVER = "2605:e000:1703:634e:c500:ae55:c2a:9d3e"
+MQTT_PATH = "test_channel"
+
+# The callback for when the client receives a connect response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_PATH)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    GPIO.output(21,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(21,GPIO.LOW)
+    time.sleep(1)
+    # more callbacks, etc
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(MQTT_SERVER, 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
