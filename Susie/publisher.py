d9d738dff09d7ee912d24807d198782c87560973
susietsx0503 <susietan@g.ucla.edu>
2019-12-08 20:51:21 +0000

    mqtt

--- Susie/publisher.py
+++ Susie/publisher.py
@@ -1,4 +1,4 @@
 import paho.mqtt.publish as publish
-MQTT_SERVER = "2605:e000:1703:634e:8883:94c6:3328:b5f5"
-MQTT_PATH = "topic/state"
+MQTT_SERVER = "192.168.31.45"
+MQTT_PATH = "/home/pi/180DA/"
 publish.single(MQTT_PATH, "light", hostname=MQTT_SERVER)
