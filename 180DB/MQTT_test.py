ae692e6e4e714e37fcc38e545b241d2f0af6eb88
susietsx0503 <susietan@g.ucla.edu>
2020-02-07 09:03:47 +0000

    speech

--- 180DB/MQTT_test.py
+++ 180DB/MQTT_test.py
@@ -24,6 +24,45 @@ import IMU
 import datetime
 import os
 import paho.mqtt.publish as publish
+
+
+##############################SPEECH RECOGNITION###########################
+fimport random
+import time
+from gtts import gTTS 
+import os 
+import speech_recognition as sr
+import vlc
+language = 'en'
+
+# set the list of words, maxnumber of guesses, and prompt limit
+WORDS = ["start", "fire", "armor", "explode", "antibiotic", "surrender"]
+NUM_GUESSES = 4
+PROMPT_LIMIT = 5
+
+# create recognizer and mic instances
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+
+# get a random word from the list
+word = random.choice(WORDS)
+
+# format the instructions string
+instructions = (
+    "The battle is ready and welcome to the weapons shop:\n"
+    "Our service include: {words}\n"
+).format(words=', '.join(WORDS), n=NUM_GUESSES)
+
+myobj = gTTS(text=instructions, lang=language, slow=False)
+myobj.save("welcome.mp3") 
+os.system("welcome.mp3")
+
+# show instructions and wait 3 seconds before starting the game
+print(instructions)
+time.sleep(10)
+
+#######################END OF SPEECH RECOGNITION#######################
+
 # If the IMU is upside down (Skull logo facing up), change this value to 1
 IMU_UPSIDE_DOWN = 0	
 
@@ -88,6 +127,53 @@ YP_10 = 0.0
 YP_11 = 0.0
 KFangleX = 0.0
 KFangleY = 0.0
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
 
 
 
@@ -434,5 +520,34 @@ while True:
 
     #slow program down a bit, makes the output more readable
     time.sleep(0.05)
+
+            # get the guess from the user
+        # if a transcription is returned, break out of the loop and
+        #     continue
+        # if no transcription returned and API request failed, break
+        #     loop and continue
+        # if API request succeeded but no transcription was returned,
+        #     re-prompt the user to say their guess again. Do this up
+        #     to PROMPT_LIMIT times
+    for j in range(PROMPT_LIMIT):
+
+        guess = recognize_speech_from_mic(recognizer, microphone)
+        if guess["transcription"]:
+            break
+        if not guess["success"]:
+            break
+
+    if guess["transcription"].lower() == "fire":
+        print("000")
+    if guess["transcription"].lower() == "start":
+        print("001")
+    if guess["transcription"].lower() == "armor":
+        print("010")
+    if guess["transcription"].lower() == "explode":
+        print("011")
+    if guess["transcription"].lower() == "antibiotic":
+        print("100")
+    if guess["transcription"].lower() == "surrender":
+        print("101")
 
 
